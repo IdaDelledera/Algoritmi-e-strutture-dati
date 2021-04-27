971fa256ea6237e88b9ca5ef6404862050a252a4
Ida <idapiadelledera@gmail.com>
2021-04-27 17:02:22 +0200

    init

--- Strutture dati/BST/main.c
+++ Strutture dati/BST/main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <malloc.h>
 
-
 typedef struct {
     struct TreeNode *left;
     struct TreeNode *right;
     int num;
-}TreeNode;
+} TreeNode;
 
 
 void inOrder(TreeNode *current);
@@ -19,19 +18,19 @@ void postOder(TreeNode *current);
 
 void nodePresent(TreeNode *root, int val);
 
-void findMin(TreeNode *root);
+TreeNode *findMin(TreeNode *root);
 
 void findMax(TreeNode *root);
 
 TreeNode *delete(TreeNode *root, int val);
 
+int altezza(TreeNode *root);
 
 int main() {
 
     TreeNode *root = NULL;
 
-
-    root=insert(root, 40);
+    root = insert(root, 40);
     insert(root, 15);
     insert(root, 12);
     insert(root, 22);
@@ -56,35 +55,44 @@ int main() {
 
     findMax(root);
 
-    delete(root, 60);
-    //delete(root, 40);
+    printf("L'altezza dell'albero e':%d\n", altezza(root));
+
+    delete(root,60);
+
     inOrder(root);
 
+    //delete(root, 12);
+
+   // inOrder(root);
+
+   // delete(root,40);
+
+    //inOrder(root);
 
     return 0;
 }
 
 
-TreeNode *insert(TreeNode *root, int val){
+TreeNode *insert(TreeNode *root, int val) {
 
     TreeNode *newNode = (TreeNode *) malloc(sizeof(TreeNode));
 
-    newNode->num=val;
-    newNode->left=NULL;
-    newNode->right=NULL;
+    newNode->num = val;
+    newNode->left = NULL;
+    newNode->right = NULL;
 
-    if(root==NULL)
+    if (root == NULL)
         return newNode;
 
-    if (val < root->num){
-        root->left=insert(root->left, val);
-    }else{
-        root->right=insert(root->right, val);
+    if (val < root->num) {
+        root->left = insert(root->left, val);
+    } else {
+        root->right = insert(root->right, val);
     }
     return root;
 }
 
-void inOrder(TreeNode *current){
+void inOrder(TreeNode *current) {
     if (current != NULL) {
         inOrder(current->left);
         printf("%d, ", current->num);
@@ -92,16 +100,16 @@ void inOrder(TreeNode *current){
     }
 }
 
-void preOrder(TreeNode *current){
-    if(current!=NULL){
+void preOrder(TreeNode *current) {
+    if (current != NULL) {
         printf("%d, ", current->num);
         preOrder(current->left);
         preOrder(current->right);
     }
 }
 
-void postOder(TreeNode *current){
-    if(current!=NULL){
+void postOder(TreeNode *current) {
+    if (current != NULL) {
         postOder(current->left);
         postOder(current->right);
         printf("%d, ", current->num);
@@ -111,63 +119,76 @@ void postOder(TreeNode *current){
 
 void nodePresent(TreeNode *root, int val) {
 
-    if(root == NULL)
+    if (root == NULL)
         printf("\nIl valore %d non esiste nel BST", val);
 
-    if(val == root->num)
+    if (val == root->num)
         printf("\nIl valore %d esiste nel BST", val);
 
-    if(val > root->num)
+    if (val > root->num)
         nodePresent(root->right, val);
-    else if(val < root->num)
+    else if (val < root->num)
         nodePresent(root->left, val);
 }
 
-void findMin(TreeNode *root){
+TreeNode *findMin(TreeNode *root) {
 
-    while (root!=NULL && root->left!=NULL){
-        root=root->left;
+    while (root != NULL && root->left != NULL) {
+        root = root->left;
     }
     printf("\nIl minimo valore del BST e' %d\n", root->num);
+    return root->num;
 }
 
-void findMax(TreeNode *root){
+void findMax(TreeNode *root) {
 
-    while (root!=NULL && root->right!=NULL){
-        root=root->right;
+    while (root != NULL && root->right != NULL) {
+        root = root->right;
     }
     printf("\nIl valore massimo all'interno del BST e' %d\n", root->num);
 }
 
-TreeNode *delete(TreeNode *root, int val){
+TreeNode* deleteNode(TreeNode *root, int val){
 
-    if (root == NULL) {
+    if (root == NULL)
         return root;
-    }
 
-    if (val < root->num) {
-        root = delete(root->left, val);
-    } else if (val > root->num) {
-        root = delete(root->right, val);
-    } else {
+    if (val < root->num)
+        root->left = deleteNode(root->left, val);
+
+    else if (val > root->num)
+        root->right = deleteNode(root->right, val);
+    else {
         if (root->left == NULL) {
-            TreeNode *tmp = root->right;
+            struct node* temp = root->right;
             free(root);
-            return tmp;
-        } else if (root->right == NULL) {
-            TreeNode *tmp = root->left;
-            free(root);
-            return tmp;
-        } else {
-            TreeNode *tmp;
-            while (root!=NULL && root->left!=NULL){
-                root=root->left;
-            }
-            tmp=root->left;
-            root->num = tmp->num;
-            root->right = delete(root->right, tmp->num);
+            return temp;
         }
-        return root;
+        else if (root->right == NULL) {
+            struct TreeNode* app = root->left;
+            free(root);
+            return app;
+        }
+
+        TreeNode* temp = findMin(root->right);
+        root->num = temp;
+        root->right = deleteNode(root->right, temp->num);
     }
+    return root;
 }
 
+
+int altezza(TreeNode *root) {
+
+    if (root == NULL)
+            return 0;
+        else {
+            int altezzaSinistra = altezza(root->left);
+            int altezzaDestra = altezza(root->right);
+
+            if (altezzaSinistra > altezzaDestra)
+                return (altezzaSinistra + 1);
+            else
+                return (altezzaDestra+1);
+        }
+}
